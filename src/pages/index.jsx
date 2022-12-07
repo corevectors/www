48e8875eacb98e3828c2a4bd1241349f05fdb510
Kerry Ivan Kurian <70291+kkurian@users.noreply.github.com>
2022-12-07 13:19:35 -0800

    Update index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,10 +14,10 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>TaxPal - Accounting made simple for small businesses</title>
+        <title>Core Vectors - Developers of Calm Technologies</title>
         <meta
           name="description"
-          content="Most bookkeeping software is accurate, but hard to use. We make the opposite trade-off, and hope you don’t get audited."
+          content="Developers of calm technologies. We help people spend time together with awareness, integrity and love."
         />
       </Head>
       <Header />
