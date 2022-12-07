1d1ccebace935f5a5103cb9e03419d69c21404b6
Kerry Ivan Kurian <70291+kkurian@users.noreply.github.com>
2022-12-07 13:23:45 -0800

    Remove cruft

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,5 @@
 import Image from 'next/image'
 
-import { Button } from '@/components/Button'
 import { NavLink } from '@/components/NavLink'
 import { Container } from '@/components/Container'
 import logoLaravel from '@/images/logos/laravel.svg'
