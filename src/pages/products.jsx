e96dc85254e7ebacd4d83302d617bc38f29987e1
Kerry Ivan Kurian <70291+kkurian@users.noreply.github.com>
2022-12-08 08:57:09 -0800

    Put in placeholder for products

---
+++ src/pages/products.jsx
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+
+import { Footer } from '@/components/Footer'
+import { Header } from '@/components/Header'
+import { Products } from '@/components/Products'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Core Vectors - Developers of Calm Technologies</title>
+        <meta
+          name="description"
+          content="Developers of calm technologies. We help people spend time together with awareness, integrity and love."
+        />
+      </Head>
+      <Header />
+      <main>
+        <Products />
+      </main>
+      <Footer />
+    </>
+  )
+}
