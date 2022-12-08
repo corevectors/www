0deceb5cd877a02737c07c8df1ffd0eaa4954b28
Kerry Ivan Kurian <70291+kkurian@users.noreply.github.com>
2022-12-07 17:24:55 -0800

    Put about page in place

---
+++ src/components/About.jsx
@@ -0,0 +1,22 @@
+import { NavLink } from '@/components/NavLink'
+import { Container } from '@/components/Container'
+
+export function About() {
+  return (
+    <Container className="pt-20 pb-16 text-center lg:pt-32">
+      <h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
+        <span className="relative whitespace-nowrap text-blue-600">
+          <span className="relative">Calm Tech</span>
+        </span>{' '}
+        for the benefit of all.
+      </h1>
+      <p className="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-slate-700">
+        We are builders of technology who help people spend time together with awareness, integrity and love.
+      </p>
+      <div className="mt-10 flex justify-center gap-x-6">
+        <NavLink href="/about">Learn more →</NavLink>
+        <NavLink href="mailto:hello@corevectors.com">Connect with us →</NavLink>
+      </div>
+    </Container>
+  )
+}
---
+++ src/pages/about.jsx
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+
+import { Footer } from '@/components/Footer'
+import { Header } from '@/components/Header'
+import { About } from '@/components/About'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Core Vectors - About</title>
+        <meta
+          name="description"
+          content="Developers of calm technologies. We help people spend time together with awareness, integrity and love."
+        />
+      </Head>
+      <Header />
+      <main>
+        <About />
+      </main>
+      <Footer />
+    </>
+  )
+}
