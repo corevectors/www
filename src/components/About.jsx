0bcd1af15a795202758bd2aed680eab17f8a0e59
Kerry Ivan Kurian <70291+kkurian@users.noreply.github.com>
2022-12-08 09:32:12 -0800

    Initial product page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,17 +2,17 @@ import {NavLink} from "@/components/NavLink";
 
 export function About() {
   return (
-    <div class="relative overflow-hidden bg-white py-16">
+    <div className="relative overflow-hidden bg-white py-16">
       <div
-        class="hidden lg:absolute lg:inset-y-0 lg:block lg:h-full lg:w-full lg:[overflow-anchor:none]">
-        <div class="relative mx-auto h-full max-w-prose text-lg"
+        className="hidden lg:absolute lg:inset-y-0 lg:block lg:h-full lg:w-full lg:[overflow-anchor:none]">
+        <div className="relative mx-auto h-full max-w-prose text-lg"
              aria-hidden="true">
-          <svg class="absolute top-12 left-full translate-x-32 transform"
+          <svg className="absolute top-12 left-full translate-x-32 transform"
                width="404" height="384" fill="none" viewBox="0 0 404 384">
             <defs>
               <pattern id="74b3fd99-0a6f-4271-bef2-e80eeafdf357" x="0" y="0"
                        width="20" height="20" patternUnits="userSpaceOnUse">
-                <rect x="0" y="0" width="4" height="4" class="text-gray-200"
+                <rect x="0" y="0" width="4" height="4" className="text-gray-200"
                       fill="currentColor"/>
               </pattern>
             </defs>
@@ -20,12 +20,12 @@ export function About() {
                   fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)"/>
           </svg>
           <svg
-            class="absolute top-1/2 right-full -translate-y-1/2 -translate-x-32 transform"
+            className="absolute top-1/2 right-full -translate-y-1/2 -translate-x-32 transform"
             width="404" height="384" fill="none" viewBox="0 0 404 384">
             <defs>
               <pattern id="f210dbf6-a58d-4871-961e-36d5016a0f49" x="0" y="0"
                        width="20" height="20" patternUnits="userSpaceOnUse">
-                <rect x="0" y="0" width="4" height="4" class="text-gray-200"
+                <rect x="0" y="0" width="4" height="4" className="text-gray-200"
                       fill="currentColor"/>
               </pattern>
             </defs>
@@ -34,19 +34,19 @@ export function About() {
           </svg>
         </div>
       </div>
-      <div class="relative px-4 sm:px-6 lg:px-8">
-        <div class="mx-auto max-w-prose text-lg">
+      <div className="relative px-4 sm:px-6 lg:px-8">
+        <div className="mx-auto max-w-prose text-lg">
           <h1>
             <span
-              class="block text-center text-lg font-semibold text-indigo-600">
+              className="block text-center text-lg font-semibold text-indigo-600">
               Calm Tech for the Benefit of All
             </span>
             <span
-              class="mt-2 block text-center text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl">
+              className="mt-2 block text-center text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl">
               More awesome. Less nonsense.
             </span>
           </h1>
-          <p class="mt-8 text-xl leading-8 text-gray-500">
+          <p className="mt-8 text-xl leading-8 text-gray-500">
             We are a group software industry veterans who{" "}
             <a target="_blank" rel="noreferrer noopener"
                className="underline text-indigo-600 font-medium"
@@ -59,7 +59,7 @@ export function About() {
             sum game.
           </p>
         </div>
-        <div class="prose prose-lg prose-indigo mx-auto mt-6 text-gray-500">
+        <div className="prose prose-lg prose-indigo mx-auto mt-6 text-gray-500">
           <p>
             In traditional terms, you can think of us as a business
             committed to building calm products that foster genuine human connection.
