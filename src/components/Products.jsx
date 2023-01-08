e726dc6b007e420547c5586fc06031a7754f5a2a
Kerry Ivan Kurian <70291+kkurian@users.noreply.github.com>
2023-01-08 15:11:47 -0800

    Revert "Post to products"
    This reverts commit 0aec2832cd469576e153032d74bfaa5ba58975d2.

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -63,7 +63,6 @@ export function Products() {
                 </span>
               </div>
               <form name="inviteList"
-                    action="/products"
                     method="POST"
                     className="mt-12 sm:mx-auto sm:flex sm:max-w-lg"
                     data-netlify="true">
