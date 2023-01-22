684e3a59e5588788124b4a9207d5bc4cbdad3292
Kerry Ivan Kurian <70291+kkurian@users.noreply.github.com>
2023-01-22 12:12:11 -0800

    Update hero text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,13 +5,10 @@ export function Hero() {
   return (
     <Container className="pt-20 pb-16 text-center lg:pt-32">
       <h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
-        <span className="relative whitespace-nowrap text-blue-600">
-          <span className="relative">calm.codes</span>
-        </span>{' '}
-        for the benefit of all.
+        Software that brings us together.
       </h1>
       <p className="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-slate-700">
-        We deliver private social networks that bring people together in real life.
+        We are creating a culture of awareness, integrity and love.
       </p>
       <div className="mt-10 flex justify-center gap-x-6">
         <NavLink href="about">Learn more →</NavLink>
